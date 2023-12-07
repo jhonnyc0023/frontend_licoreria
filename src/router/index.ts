64abcfea8fc0b26a6b56ca892399c39664a12e85
jhonny condori <jhonnyc0023@gmail.com>
2023-12-07 17:01:05 -0400

    prueba2

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LoginView from '@/views/LoginView.vue'
+import LoginView from '../views/AboutView.vue'
 
 import { useAuthStore } from '@/stores'
 import { getTokenFromLocalStorage } from '@/helpers'
